f123a7fdaf85c9b878a269ec8d9d15500e085716
daresh887 <daresh.hanganu@gmail.com>
2023-08-27 10:32:34 +0300

    Updates

--- daVinci/main_app/views.py
+++ daVinci/main_app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Course
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Academia daVinci")
+    courses = Course.objects.all()
+    return render(request, "main_app/index.html", {
+        "courses": courses
+    })
+
+def page(request, page_slug):
+    return HttpResponse(page_slug)
